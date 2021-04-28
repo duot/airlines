16b2464eeecf0eb5ba481fa0fc28a1baa23871d7
James Duot <duot.jim@gmail.com>
2021-04-28 12:00:16 -0700

    displaying routes raw in table rows

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import './App.css';
+import data from './data'
 
-const App = () => (
-  <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
-)
+const App = () => {
+  const [routes, setRoutes] = useState([])
+
+  useEffect(() => {
+    setRoutes(data.routes)
+  }, [])
+
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        <p>
+          Welcome to the app!
+        </p>
+        <table>
+          {routes.map(({ airline, src, dest }) => 
+            <tr>`{airline} {src}, {dest}`</tr>
+          )}
+        </table>
+      </section>
+    </div>
+  )
+}
 
 export default App;
